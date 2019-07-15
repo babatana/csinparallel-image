7017da6702dc009d1c2687f5436f414abc42884b
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-07-15 18:01:17 -0500

    Update

--- files/pullUpdates.bash
+++ files/pullUpdates.bash
@@ -2,4 +2,4 @@
 
 /usr/local/bin/ansible-pull -o \
 -U https://gitlab+deploy-token-12:sErpRQP96JzfVponpBh-@stogit.cs.stolaf.edu/narvae1/hd-image.git \
--e imgVersion=`cat /usr/share/HD/version` | /usr/bin/logger -t cron-ansible-pull
+-e imgVersion=`cat /usr/share/HD/version`
