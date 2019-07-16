8fa111aa5bf497a560a6a768a8f16782a5be5d1e
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-07-16 14:39:26 -0500

    2.0.0.12

--- files/hd-image.bash
+++ files/hd-image.bash
@@ -6,10 +6,52 @@ show_help() {
     echo "Options:"
     echo "-h,   --help          show this help message"
     echo "-v,   --version       show the current version of the image"
+    echo "-i,   --info          show information about this Pi"
+    echo "      --report        send info to PiTracker server"
     echo "      --update        check for image updates"
     echo "                      (this happens automatically at startup"
     echo "                       and at 5am each day)"
-    exit 1
+    exit 0
+}
+
+info() {
+    IMAGEVER=`cat /usr/share/HD/version`
+    SERIAL=`cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2`
+    IP=`hostname -I`
+    ipa=1
+    for ip in $IP
+    do
+        j=`echo $ip | cut -d ' ' -f $ipa`
+        if echo $j | grep "10.44." &> /dev/null
+        then
+            IP=$j
+            break
+        fi
+    done
+    MAC=`ifconfig wlan0 | grep ether | sed 's/  \+/ /g' | cut -d ' ' -f 3`
+    SDSERIAL=`cat /sys/block/mmcblk0/device/cid`
+    HARDREV=`cat /proc/cpuinfo | grep Revision | cut -d ' ' -f 2`
+    OWNER=`cat /etc/owner`
+
+    echo "Image Version:        $IMAGEVER"
+    echo "Hardware Revision:    $HARDREV"
+    echo "Pi Serial Number:     $SERIAL"
+    echo "SD Serial Number:     $SDSERIAL"
+    echo "WiFi IP:              $IP"
+    echo "WiFi MAC:             $MAC"
+    echo "Owner:                $OWNER"
+}
+
+report() {
+    /usr/share/HD/PiTracker.bash
+}
+
+update() {
+    /usr/local/bin/ansible-pull -o \
+    -U https://gitlab+deploy-token-12:sErpRQP96JzfVponpBh-@stogit.cs.stolaf.edu/narvae1/hd-image.git \
+    -e imgVersion=`cat /usr/share/HD/version`
+
+    report
 }
 
 while test $# -gt 0
@@ -25,7 +67,18 @@ do
             ;;
         --update)
             shift
-            /usr/share/HD/PiTracker.bash
+            update
+            exit 0
+            ;;
+        -i|--info)
+            shift
+            info
+            exit 0
+            ;;
+        --report)
+            shift
+            report
+            exit 0
             ;;
         *)
             show_help
