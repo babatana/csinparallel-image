e8a2c0b7b0470ad7ca701b528c8b7bddd7db79b0
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-07-16 13:52:39 -0500

    2.0.0.12

--- files/hd-image.bash
+++ files/hd-image.bash
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+show_help() {
+    echo "Hardware Design Image Tool"
+    echo
+    echo "Options:"
+    echo "-h,   --help          show this help message"
+    echo "-v,   --version       show the current version of the image"
+    echo "      --update        check for image updates"
+    echo "                      (this happens automatically at startup"
+    echo "                       and at 5am each day)"
+    exit 0
+}
+
 while test $# -gt 0
 do
     case "$1" in
+        -h|--help)
+            show_help
+            ;;
         -v|--version)
+            shift
             cat /usr/share/HD/version
             exit 0
             ;;
         --update)
+            shift
             /usr/share/HD/pullUpdates.bash
             ;;
-        -h|--help|*)
-            echo "Hardware Design Image Tool"
-            echo
-            echo "Options:"
-            echo "-h,   --help          show this help message"
-            echo "-v,   --version       show the current version of the image"
-            echo "      --update        check for image updates"
-            echo "                      (this happens automatically at startup"
-            echo "                       and at 5am each day)"
-            ;;
+
     esac
 done
